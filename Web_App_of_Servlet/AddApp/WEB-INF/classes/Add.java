3db32dc10385be0831df9ad4a9585b9d4f0b1855
Amit Kumar Singh <70219325+amitksingh1697@users.noreply.github.com>
2021-02-14 15:13:49 +0530

    Add files via upload

---
+++ Web_App_of_Servlet/AddApp/WEB-INF/classes/Add.java
@@ -0,0 +1,32 @@
+import javax.servlet.*;
+import java.io.*;
+public class Add implements Servlet{
+public void init(ServletConfig sc){}
+public void service(ServletRequest sreq,ServletResponse sres){
+PrintWriter out=null;
+try{
+String s1=sreq.getParameter("num1");
+String s2=sreq.getParameter("num2");
+int n1=Integer.parseInt(s1);
+int n2=Integer.parseInt(s2);
+int n=n1+n2;
+sres.setContentType("text/html");
+out=sres.getWriter();
+out.println("<html><boby>");
+out.println("<h1>My First Web Application</h1>");
+out.println("Addition of <b>"+n1+"</b> and <b>"+n+"</b> = <b>"+n+"</b>");
+out.println("</boby></html>");
+}
+catch(Exception ex)
+ {
+	out.println(ex);
+ }
+}
+public void destroy(){}
+public ServletConfig getServletConfig(){
+	return null;
+ }
+public String getServletInfo(){
+	return null;
+ }
+}
